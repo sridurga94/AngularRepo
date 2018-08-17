2eff0d0887bc00072a8300ea4ef48b18e8dfc184
sridurga94 <sridurgagundumogula@gmail.com>
2018-08-17 22:24:31 +0530

    components added

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles: ['h3{color: dodgerblue;}']
 })
 export class AppComponent {
   title = 'my-first-app';
